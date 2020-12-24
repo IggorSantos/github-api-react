6f18b4c6f8b7e85bd2491370bcc480f4c22361d0
Igor Santos <igor.sagitarius@gmail.com>
2020-12-24 16:00:17 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
 
 function App() {
+  const [users,setUsers] = useState([])
+  const [input,setInput] = useState([])
+  const [search,setSearch] = useState([])
+
+  useEffect( () => {
+    async function getUsers(){
+      const response = await fetch(`https://api.github.com/users/${search}`).then((responseServer) => {
+        if(responseServer.ok){
+          return responseServer.json()
+        }else{
+          return Promise.reject("User not found")
+        }
+      })
+      setUsers(response)
+      console.log(users)
+
+    }
+    getUsers()
+  },)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <input type="text" onChange={event => setInput(event.target.value)} />
+    <button type="submit" onClick={() => setSearch(input)} >
+      Pesquisar
+    </button>
+{users.login}
+
     </div>
   );
 }
